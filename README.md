e0aa5b3737c704071da1819c25ee56e77b055b76
Attila Maczak <attila@maczak.hu>
2015-01-28 22:37:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+gobble-rename
+=============
+
+Rename files using JS `.replace` method.
+
+## Installation
+
+First, you need to have gobble installed - see the [gobble readme](https://github.com/gobblejs/gobble) for details. Then,
+
+```bash
+npm i -D gobble-rename
+```
+
+## Usage
+
+**gobblefile.js**
+
+```js
+var gobble = require( 'gobble' );
+module.exports = gobble('rename' ).transform('rename', {from: 'searchstring',
+to: 'replacewith');
+```
+
+This will look for files with filename containing 'searchstring' and renames
+them with 'replacewith'.
+
+a/b/c/searchstring.css > a/b/c/replacewith.css
+
+## License
+
+MIT. Copyright 2015 Attila Maczak
+Based on gobble's builtins/include.js.
+MIT Licensed. Copyright 2014 Rich Harris.
---
+++ index.js
@@ -0,0 +1,48 @@
+var path = require( 'path' ),
+    sander = require( 'sander' ),
+    path = require( 'path' ),
+    fs = require('graceful-fs'),
+    symlinkOrCopy = require( 'symlink-or-copy' ).sync;
+
+module.exports = rename;
+function rename(inputdir, outputdir, options, callback) {
+  console.log(inputdir);
+  function processdir (dir, cb) {
+    fs.readdir(dir, function (err, files) {
+      var remaining = files.length, result = [], check;
+      if (err) return cb(err);
+      // Empty dir?
+      if (!remaining) { cb(null, result); }
+      check = function () {
+        if (!--remaining) { cb(null, result); }
+      };
+
+      function handleResult (err) {
+        if (err) return cb(err);
+        check();
+      }
+
+      files.forEach(function (filename) {
+        var filepath = dir + path.sep + filename;
+        var destpath = filepath.replace(inputdir, outputdir)
+                               .replace(options.from, options.to);
+        fs.stat(filepath, function (err, stats) {
+          if (err) return cb( err );
+          if ( stats.isDirectory() ) {
+            processdir(filepath, handleResult);
+          } else {
+            sander.mkdirSync(path.dirname(destpath));
+            try {
+              symlinkOrCopy(filepath, destpath);
+              check();
+            } catch (e) {
+              cb(e);
+            }
+          }
+        });
+      })
+    });
+  };
+  processdir(inputdir, callback);
+}
+
